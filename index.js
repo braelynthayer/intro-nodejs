68d0d02932c33ed86fc92a17b221e3ee897e585f
lwooden <lowell.wooden@gmail.com>
2021-05-17 20:52:45 -0400

    low updates

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Hello World!")
+
+// Hey Brae! It's Low and I just "pulled" in your code from your GitHub 
+// Let's add some more functionality to your awesome project!
+
+projectOwner = "Braelyn"
+contributor = "Low"
+const message = (owner, contributor) => `Hey ${owner}! My name is ${contributor} and I just saw your new project on GitHub! Your doing some great stuff! Mind if contribute?`
+
+console.log(message(projectOwner, contributor))
